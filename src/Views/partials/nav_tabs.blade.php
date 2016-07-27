e00cf447dc93310bf946da3e2c72065dd987ffa1
Yadda <yadda@yadda.co.uk>
2016-07-27 10:26:42 +0000

    Move active class from a to li on crud autoform nav-tabs

--- src/Views/partials/nav_tabs.blade.php
+++ src/Views/partials/nav_tabs.blade.php
@@ -1,8 +1,8 @@
 @if(count($tabs) > 1)
   <ul class="nav nav-tabs">
     @foreach($tabs as $tab_name => $x)
-       <li>
-         <a href="#tab-{{ strtolower(preg_replace('#[^a-zA-Z0-9]+#', '-', $tab_name)) }}" data-toggle="tab"{{ (reset($tabs) === $x) ? ' class=active' : '' }}>
+       <li{{ (reset($tabs) === $x) ? ' class=active' : '' }}>
+         <a data-toggle="tab" href="#tab-{{ strtolower(preg_replace('#[^a-zA-Z0-9]+#', '-', $tab_name)) }}">
            {{ $tab_name }}
          </a>
        </li>
