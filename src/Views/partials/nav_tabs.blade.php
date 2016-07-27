5c0e6b937ed36c62a29e2087f56240999f560a95
Jake Gully <jake@yadda.co.uk>
2016-07-27 14:33:54 +0100

    Update autoform tabs for bootstrap 4

--- src/Views/partials/nav_tabs.blade.php
+++ src/Views/partials/nav_tabs.blade.php
@@ -1,8 +1,8 @@
 @if(count($tabs) > 1)
   <ul class="nav nav-tabs">
     @foreach($tabs as $tab_name => $x)
-       <li{{ (reset($tabs) === $x) ? ' class=active' : '' }}>
-         <a data-toggle="tab" href="#tab-{{ strtolower(preg_replace('#[^a-zA-Z0-9]+#', '-', $tab_name)) }}">
+       <li class="nav-item">
+         <a class="nav-link {{ (reset($tabs) === $x) ? ' active' : '' }}" data-toggle="tab" href="#tab-{{ strtolower(preg_replace('#[^a-zA-Z0-9]+#', '-', $tab_name)) }}">
            {{ $tab_name }}
          </a>
        </li>
